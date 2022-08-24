a47bf174873a2e19d0ca7acc88f1a1fa2c1d9e78
SunFulin <YS.sunfulin@h3c.com>
2022-08-24 17:46:59 +0800

    提交

--- src/view/workFlow/flowEngine/CustomForm/index.js
+++ src/view/workFlow/flowEngine/CustomForm/index.js
@@ -1,22 +1,24 @@
 /*
  * @Author: sunFulin
  * @Date: 2022-08-23 17:12:37
- * @LastEditTime: 2022-08-24 13:45:57
+ * @LastEditTime: 2022-08-24 17:44:31
  */
 import React, { memo, useState } from "react";
+import { Tag } from "antd";
 import { ReactSortable } from "react-sortablejs";
+import { formItemData } from "./config";
+import "./index.scss";
 export default memo(() => {
-  const [state, setState] = useState([
-    { id: 1, name: "shrek" },
-    { id: 2, name: "fiona" },
-  ]);
+  const [state, setState] = useState(formItemData);
   return (
     <ReactSortable list={state} setList={setState}>
-      {state.map((item) => (
-        <div key={item.id} style={{ border: "1px solid red" }}>
-          {item.name}
-        </div>
-      ))}
+      <div className="ItemBox">
+        {state.map((item) => (
+          <div className="Item" key={item.name} style={{ marginTop: 10 }}>
+            <Tag>{item.label + "-" + item.name}</Tag>
+          </div>
+        ))}
+      </div>
     </ReactSortable>
   );
 });
